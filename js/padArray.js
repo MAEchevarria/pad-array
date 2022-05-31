425caa5a82ed0c75a9df2167b1866e7e49b72544
Michael Echevarria <michael.echevarria@live.com>
2022-05-31 09:46:31 -0500

    updated code for js

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,14 @@
 // REMEMBER TO PSEUDOCODE
 const pad = (array, minSize, value=null) => {
-
+// function pad(array, minSize, value = null) {
+    // establish new array from arguments being passed to function
+    let newArr = array;
+    // while the array is shorter than the desired length, add defined value to end of array
+    while (newArr.length < minSize) {
+        newArr.push(value);
+    }
+    // return array;
+    return newArr;
 }
+console.log(pad([1,2,3], 5));
+console.log(pad([1,2,3], 5, "apple"));
